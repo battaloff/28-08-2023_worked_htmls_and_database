0302a90b2d2ed3b0bfc8752d82789278686ef6d4
battaloff <battaloff@mail.ru>
2023-11-22 16:36:10 +0500

    22-11-2023

--- politext_tasks/sql_delete_by_filename.py
+++ politext_tasks/sql_delete_by_filename.py
@@ -4,13 +4,16 @@ import sqlite3
 conn = sqlite3.connect(r'D:\###TASKS\newproject\politext_tasks\db.sqlite3')
 cursor = conn.cursor()
 
+# Значение, которое вы хотите использовать в качестве условия
+file_name_to_delete = '775-635_16_ISHONCH_2980 - SAKATA KATALOG.pdf'
+
 # Удаление строк из таблицы
 delete_query = """
 DELETE FROM tasks_politext_tabletask
-WHERE file_name = '1030-790_4_DAVR P_P+Z_ALMAZ-ISNUR_960x600_4+0_5000+100tir.pdf'
+WHERE file_name = ?
 """
 
-cursor.execute(delete_query)
+cursor.execute(delete_query, (file_name_to_delete,))
 conn.commit()
 
 # Закрытие соединения с базой данных
